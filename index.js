e78d9b18cb7cd0c80899ab073f175e71baf25062
mshekerotokiti <sharontokiti@gmail.com>
2023-01-09 16:08:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const day = 'Tuesday';
+
+switch (day){
+    case 'Monday':
+        console.log(`Cook Chapati and lentils`)
+        console.log(`Finish all the house chores`)
+    break;
+
+    case 'Tuesday':
+        console.log(`Attend a meeting with Mr. Benson`)
+        console.log(`Take My son to School at 8am`)
+        console.log(`Face routine`)
+    break;
+
+    case 'Wednesday':
+        console.log(`Go to the salon`)
+     break;
+
+     case 'Thursday':
+        console.log(`Record Youtube videos`)
+     break;
+
+     case 'Friday':
+        console.log(`Go out for a drink`)
+        console.log(`Go to the massage place`)
+     break;
+     case 'Saturday':
+        case 'Sunday':
+            console.log(`Relaxing and resting`)
+     break;
+
+}
